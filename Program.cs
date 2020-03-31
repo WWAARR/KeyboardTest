2e2e791961ee66aba1d6b8d677e8e5a00fb07196
WWAARR <wwaarrtop@gmail.com>
2020-03-31 21:42:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            var rand = new Random();
+            int a = 1;
+            for (int i = 0; i < a; i++)
+            {
+            Console.Write((char)rand.Next(65, 90));//97,122
+            }
+            string S = Console.ReadLine();
+            Console.Write("\n");
+            var F = Convert.ToString(S);
+            */
+            Random rand = new Random();
+            int[] arr = new int[20];
+            //int[] arr1 = new int[20];
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (i % 3 == 0 && i > 0)
+                {
+                    arr[i] = '_';
+                    i++;
+                }
+                arr[i] = rand.Next(97, 122);//Little
+                //arr1[i] = rand.Next(65, 90);//Large
+            }
+            for (int i = 0; i < 1; i++)
+            {
+               Console.Write((char)arr[i]);
+            }
+            int S = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("\n");
+            if (S == Convert.ToChar(arr[0]))
+            {
+                Console.WriteLine("Вы быстры!");
+            }
+            else
+            {
+                Console.WriteLine($"\aВы не правы. :C");
+            }
+        }
+    }
+}
